6fc151dcf895cad99675c44e599c818d017a1f87
hermanyas <120398512+hermanyas@users.noreply.github.com>
2023-02-20 19:02:31 +0400

    debug

--- startos.sh
+++ startos.sh
@@ -15,3 +15,5 @@ TOKEN=5449476960:AAGRxyle0BpDzOXAyaZVpBTi0pSGdjkVPhc
 MESSAGE='`'"$(cat /var/log/cloudflared.log | head -8 | grep trycloudflare.com | tail -1 | awk '{print $4}' | sed 's~http[s]*://~~g')"'`'
 
 curl --data parse_mode=MarkdownV2 --data chat_id=$ID --data text="$MESSAGE" --request POST https://api.telegram.org/bot$TOKEN/sendMessage 2>&1 &>/dev/null
+
+bash
